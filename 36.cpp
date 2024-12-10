94de0bf4c8c7e8954b8c088f4f6daa513d1b70b0
Ralliyah889 <ralliyahrushdah@gmail.com>
2024-12-10 11:22:59 +0530

    Add files via upload

---
+++ 36.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+int main()
+{ 
+    char str1[10]="hello";
+    char str2[10]="world";
+    cout<<str1<<"\n";
+    cout<<str2;
+    return 0;
+}
